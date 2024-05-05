097793a0b57f95e243e875910839d7570451801a
Lain0x15 <max0x15@outlook.com>
2024-05-05 18:56:21 +0300

    V9.3.1

--- src/lolzeAutoUP.py
+++ src/lolzeAutoUP.py
@@ -321,6 +321,6 @@ class lolzeAutoUP:
                         params = self.__config['modules'][module]['params']
                         self.__modules[module]['run'](**params)
             except Exception as err:
-                self.__log(err, logLevel='error')
+                self.__log(f'Ошибка: {err}. Перезапускаюсь.', logLevel='error')
             finally:
                 pass
