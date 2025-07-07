d27b9caeb3bcc2e4192962eca67ba7369d1526ba
Lain0x15 <max0x15@outlook.com>
2025-07-07 23:57:42 +0300

    fx

--- modules/autoNotice/configSchema.py
+++ modules/autoNotice/configSchema.py
@@ -3,7 +3,7 @@ from schema import Schema, SchemaError, And, Or, Optional
 configSchema = Schema ({
     'pagesPerUrl': int,
     Optional('save_in_file'): bool,
-    Optional('retry_page_count'): bool,
+    Optional('retry_page_count'): int,
     'marketURLs': [{
         'url': str,
         'searchTransactionsTitle': [str]
